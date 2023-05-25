ab1cfd1ea8630df3f44f24d870653e5bb0f989a2
Silvia <silvia.garbas@gmail.com>
2023-05-25 22:32:55 +0200

    Modified display and info component

--- src/components/info/info.tsx
+++ src/components/info/info.tsx
@@ -1,3 +1,14 @@
-export function Info(){
-  return
+import { useContext } from "react";
+import { Context } from "../../context/context";
+
+export function Info() {
+  const {
+    phoneContext: { handleChange },
+  } = useContext(Context);
+
+  return (
+    <>
+      <span className={handleChange()}>Calling...</span>
+    </>
+  );
 }
