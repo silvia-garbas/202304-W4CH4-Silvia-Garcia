305522302fdff8c23a1636503bd8287b8c828908
Silvia <silvia.garbas@gmail.com>
2023-05-25 22:26:29 +0200

    Added Action component

---
+++ src/components/actions/Actions.tsx
@@ -0,0 +1,13 @@
+import { Action } from "../action/Action";
+import { Display } from "../display/Display";
+
+export function Actions() {
+  return (
+    <>
+      <div className="actions">
+        <Display></Display>
+        <Action></Action>
+      </div>
+    </>
+  );
+}
